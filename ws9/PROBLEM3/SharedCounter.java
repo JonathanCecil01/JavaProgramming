9f54d54a33e523dd2876c7564b07285ff76ff628
Jonathan Cecil <82434403+JonathanCecil01@users.noreply.github.com>
2022-09-27 09:59:28 +0530

    Add files via upload

--- ws9/PROBLEM3/SharedCounter.java
+++ ws9/PROBLEM3/SharedCounter.java
@@ -1,10 +1,26 @@
 package PROBLEM3;
+
+class Global
+{
+    static int GlobalX = 0;
+}
+
 class Increment extends Thread
 {
-    public void run();
+    public void run()
+    {
+        for(int i=0;i<10;i++)
+            Global.GlobalX++;
+      System.out.println(Global.GlobalX);
+    }
 }
 
 public class SharedCounter {
-    int n=10;
+    public static void main(String[]args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Increment thread = new Increment();
+            thread.start();
+        }
 
+    }
 }
